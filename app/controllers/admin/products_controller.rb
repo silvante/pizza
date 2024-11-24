7f72cb4349657c3979e6f5fa1f369cd817dfbd4a
silvante <khamidov.ko@gmail.com>
2024-11-24 19:35:16 +0500

    edit and new pages # product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -25,7 +25,7 @@ class Admin::ProductsController < AdminController
 
     respond_to do |format|
       if @admin_product.save
-        format.html { redirect_to @admin_product, notice: "Product was successfully created." }
+        format.html { redirect_to admin_products_path, notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @admin_product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class Admin::ProductsController < AdminController
   def update
     respond_to do |format|
       if @admin_product.update(admin_product_params)
-        format.html { redirect_to @admin_product, notice: "Product was successfully updated." }
+        format.html { redirect_to admin_products_path, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @admin_product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -65,6 +65,6 @@ class Admin::ProductsController < AdminController
 
     # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.expect(admin_product: [ :title, :desc, :is_active, :price, :is_hit, :category_id, :image ])
+      params.expect(product: [ :title, :desc, :is_active, :price, :is_hit, :category_id, :image ])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
+  has_one_attached :image
   belongs_to :category
+
+  def thumbnail
+    image.variant(resize_to_limit: [ 100, 100 ])
+  end
 end
