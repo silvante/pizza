792c2ad3c9560162eaf6107bf55ccb07c7c16fb7
silvante <khamidov.ko@gmail.com>
2024-11-25 02:30:27 +0500

    devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def show_header?
+    # Customize this array to include actions where the header should be hidden
+    hidden_devise_actions = %w[new create edit update]
+    hidden_devise_controllers = %w[registrations sessions confirmations passwords unlocks]
+
+    !hidden_devise_controllers.include?(controller_name) ||
+      !hidden_devise_actions.include?(action_name)
+  end
 end
