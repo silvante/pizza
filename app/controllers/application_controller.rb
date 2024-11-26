6be1936b3fd71950eadc4b6a07d9982d5a2bbba1
silvante <khamidov.ko@gmail.com>
2024-11-26 22:39:42 +0500

    start of Ordering system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :categories
+
+  def categories
+    Category.all
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,5 @@ Rails.application.routes.draw do
   get "/admin", to: "admin#index"
 
   resources :users
+  resources :cart
 end
