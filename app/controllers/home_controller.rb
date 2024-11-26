65cfc90e79e643d581a89d325d26ee4ab740d9aa
silvante <khamidov.ko@gmail.com>
2024-11-27 02:16:17 +0500

    add to cart function is working more then needed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @products = Product.all
+    @order = Order.find_by(user: current_user, status: "pending")
+    @checking_ids = @order.order_products.pluck(:product_id)
   end
 end
