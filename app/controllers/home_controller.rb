11fb1bf1f3822e7024b59c9056e8fdcf37581487
silvante <khamidov.ko@gmail.com>
2024-11-27 20:48:53 +0500

    start

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
     @categories = Category.all
     @products = Product.all
     @order = Order.find_by(user: current_user, status: "pending")
-    @checking_ids = @order.order_products.pluck(:product_id)
+    if @order.present?
+      @checking_ids = @order.order_products.pluck(:product_id)
+    end
   end
 end
