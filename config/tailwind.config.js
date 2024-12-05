ed45ac44c05f3b08bea37fe61a6f9baf34a3022c
silvante <khamidov.ko@gmail.com>
2024-12-05 15:40:42 +0500

    adding daisy UI

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -25,5 +25,9 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"], // Example themes
+  },
 }
