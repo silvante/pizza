61fa3d7a3ac64815e63c2b414fd5a484906d727f
silvante <khamidov.ko@gmail.com>
2024-12-07 14:35:15 +0500

    just creating some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+User.create(name: "Manager", email: "admin@we.com", password: "admin1234", status: "admin", mobile: "333038282", born_in: 2000)
