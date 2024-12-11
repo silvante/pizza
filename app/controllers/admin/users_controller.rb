942aeea5c434b6e837ae73cb08a33ac3cd185521
silvante <khamidov.ko@gmail.com>
2024-12-12 01:48:36 +0500

    fully functional admin::users pages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,8 +7,25 @@ class Admin::UsersController < AdminController
     user = User.find(params[:id])
     if user.status == "client"
       @user = user
+      @orders = @user.order.select { |order| order.status == "delivered" }
+      @order_length = @orders.length
+      @product_length = @orders.sum { |o| o.order_products.sum { |op| op.amount } }
+      @total_spent = @orders.sum { |o| o.order_products.sum { |op| op.amount * op.product.price } }
     else
       redirect_to admin_users_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.status != "client"
+      redirect_to admin_users_path
+    else
+      @user.destroy!
+      respond_to do |format|
+        format.html { redirect_to admin_users_path, notice: "Mijon Muafffaqiyatli ochirildi" }
+        format.json { head :no_content }
+      end
     end
   end
 end
