6bf9386003fc9ae546462ddd2ff10cff1f713dba
silvante <khamidov.ko@gmail.com>
2024-12-19 03:05:22 +0500

    fix

--- app/javascript/controllers/count_controller.js
+++ app/javascript/controllers/count_controller.js
@@ -2,6 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="count"
 export default class extends Controller {
+  static targets = ["output", "input"]
+  static values = { count: Number }
+
   connect() {
+    this.countValue = 1; // Default quantity
+    this.updateFormInput();
+  }
+
+  increment() {
+    this.countValue++;
+    this.updateUI();
+  }
+
+  decrement() {
+    if (this.countValue > 1) {
+      this.countValue--;
+      this.updateUI();
+    }
+  }
+
+  updateUI() {
+    this.outputTarget.textContent = this.countValue;
+    this.updateFormInput();
+  }
+
+  updateFormInput() {
+    this.inputTarget.value = this.countValue;
   }
 }
