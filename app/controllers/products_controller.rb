4b60cec7808ec5764c7c7d543a1ca70490c12588
silvante <khamidov.ko@gmail.com>
2025-01-24 07:39:54 +0500

    fixing product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
+    @related_products = Product.where(category: @product.category)
   end
 end
